434004bd00bb51f200ccfd241ee6540cfbff9468
caolingyun <caolingyun@163.com>
2021-08-13 14:33:50 +0800

    hot-fix commit1

--- rabbitmq/src/main/java/com/example/rabbitmq/one/test.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/one/test.java
@@ -9,5 +9,8 @@ package com.example.rabbitmq.one;
 public class test {
     public static void main(String[] args) {
         System.out.println("git@!");
+        System.out.println("git2");
+        System.out.println("git3");
+        System.out.println("git4");
     }
 }
